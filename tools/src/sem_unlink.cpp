d0faeed4d16005996bbf9b5cc1bf7b98daaf66fe
Antonio Sanchez <antonios@ece.ubc.ca>
2017-05-03 12:54:42 -0700

    modified unlink tools to accept pipe input

--- tools/src/sem_unlink.cpp
+++ tools/src/sem_unlink.cpp
@@ -1,31 +1,52 @@
 #include "cpen333/os.h"
 
-#include <semaphore.h>
 #include <iostream>
-#include <cerrno>    // error numbers
-
 
 #ifdef POSIX
+#include <semaphore.h>
+#include <unistd.h>
+#include <cerrno>    // error numbers
+#include <string>
+
+void __sem_unlink(const char* name) {
+  int status = sem_unlink(name);
+  if (status != 0 && errno == EACCES) {
+    std::cerr << "Failed to unlink semaphore " << name << ": ACCESS DENIED" << std::endl;
+  }
+}
+
 int main(int argc, char* argv[]) {
 
-  if (argc < 2) {
-    std::cout << "Usage:" << std::endl;
-    std::cout << argv[0] << " <list of semaphore IDs...>" << std::endl;
-    std::cout << "\t\tSemaphore IDs can be found on OSX by using the command" << std::endl;
-    std::cout << "\t\t\tlsof | grep PSXSEM | awk '{print $7}'" << std::endl;
-  }
-
-  // try to unlink all semaphores provided as arguments
-  for (int i=1; i<argc; ++i) {
-    int status = sem_unlink(argv[i]);
-    if (status != 0 && errno == EACCES) {
-      std::cerr << "Failed to unlink semaphore " << argv[i] << ": ACCESS DENIED" << std::endl;
+  // command-line arguments
+  if (argc > 2) {
+    // try to unlink all semaphores provided as arguments
+    for (int i=1; i<argc; ++i) {
+      __sem_unlink(argv[i]);
     }
   }
+  // read from pipe
+  else if (!isatty(fileno(stdin))) {
+    std::string name;
+    while (std::cin >> name) {
+      __sem_unlink(name.c_str());
+    }
+  }
+  // print usage
+  else {
+    std::cout << "Usage:" << std::endl;
+    std::cout << argv[0] << " <list of semaphore IDs...>" << std::endl;
+    std::cout << "\tSemaphore IDs can be found on OSX by using the command" << std::endl;
+    std::cout << "\t\tlsof | grep PSXSEM'" << std::endl;
+    std::cout << "\tThese can be piped in directly, i.e." << std::endl;
+    std::cout << "\t\tlsof | grep PSXSEM | awk '{print $7}' | " << argv[0] << std::endl;
+  }
+
   return 0;
 }
 #else
 int main(int argc, char* argv[]) {
+
+  std::cin.peek();
   std::cout << "Cannot unlink semaphores on Windows" << std::endl;
   return 0;
 }
--- tools/src/shm_unlink.cpp
+++ tools/src/shm_unlink.cpp
@@ -1,27 +1,46 @@
 #include "cpen333/os.h"
-
-#include <sys/mman.h>
 #include <iostream>
-#include <cerrno>    // error numbers
 
 
 #ifdef POSIX
+#include <sys/mman.h>
+#include <unistd.h>
+#include <cerrno>    // error numbers
+#include <string>
+
+void __shm_unlink(const char* name) {
+  int status = shm_unlink(name);
+  if (status != 0 && errno == EACCES) {
+    std::cerr << "Failed to unlink shared memory " << name << ": ACCESS DENIED" << std::endl;
+  }
+}
+
 int main(int argc, char* argv[]) {
 
-  if (argc < 2) {
-    std::cout << "Usage:" << std::endl;
-    std::cout << argv[0] << " <list of shared memory IDs...>" << std::endl;
-    std::cout << "\t\tShared memory IDs can be found on OSX by using the command" << std::endl;
-    std::cout << "\t\t\tlsof | grep PSXSHM | awk '{print $7}'" << std::endl;
-  }
-
-  // try to unlink all semaphores provided as arguments
-  for (int i=1; i<argc; ++i) {
-    int status = shm_unlink(argv[i]);
-    if (status != 0 && errno == EACCES) {
-      std::cerr << "Failed to unlink shared memory " << argv[i] << ": ACCESS DENIED" << std::endl;
+   // command-line arguments
+  if (argc > 2) {
+    // try to unlink all semaphores provided as arguments
+    for (int i=1; i<argc; ++i) {
+      __shm_unlink(argv[i]);
     }
   }
+  // read from pipe
+  else if (!isatty(fileno(stdin))) {
+    std::string name;
+    while (std::cin >> name) {
+      __shm_unlink(name.c_str());
+    }
+  }
+  // print usage
+  else {
+    std::cout << "Usage:" << std::endl;
+    std::cout << argv[0] << " <list of shared memory IDs...>" << std::endl;
+    std::cout << "\tShared memory IDs can be found on OSX by using the command" << std::endl;
+    std::cout << "\tlsof | grep PSXSHM | awk '{print $7}'" << std::endl;
+    std::cout << "\tThese can be piped in directly, i.e." << std::endl;
+    std::cout << "\t\tlsof | grep PSXSHM | awk '{print $7}' | " << argv[0] << std::endl;
+  }
+
   return 0;
 }
 #else
